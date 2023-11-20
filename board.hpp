bc67d96937416c1629c2b2e9bca15dbab66c9a68
gustavo hanke <gustavo.hanke1@outlook.com>
2023-11-20 14:50:56 -0300

    changed things, mainly methods

--- board.hpp
+++ board.hpp
@@ -1,30 +1,42 @@
-enum GameResult {
+enum GameResult
+{
     GameResult_Player1_Wins,
     GameResult_Player2_Wins,
     GameResult_Draw,
-    GameResult_NotDone,
-    GameResult_Invalid
+    GameResult_NotDone
 };
 
-struct GameState {
+struct GameState
+{
     // cada coluna, usando bit field pra economizar o maximo possivel de memoria
-    unsigned col1: 4;
-    unsigned col2: 4;
-    unsigned col3: 4;
-    unsigned col4: 4;
+    unsigned col1 : 4;
+    unsigned col2 : 4;
+    unsigned col3 : 4;
+    unsigned col4 : 4;
     // numero de elementos em cada coluna
-    unsigned len1: 3;
-    unsigned len2: 3;
-    unsigned len3: 3;
-    unsigned len4: 3;
-    unsigned pading : 4 = 0; //alinhamento de memória, o compilador não gosta de coisas com 28 bits.
+    unsigned len1 : 3;
+    unsigned len2 : 3;
+    unsigned len3 : 3;
+    unsigned len4 : 3;
+    unsigned turn : 1;       // indica quem deve ser o próximo a jogar.
+    unsigned pading : 3 = 0; // alinhamento de memória, o compilador não gosta de coisas com 29 bits.
 
-    // 'joga' neste tabuleiro na coluna e turno determinados, gerando o filho
-    GameState play(unsigned col, bool turn);
+    // joga na coluna especificada, gerando o próximo tabuleiro, retorna falso se não for uma jogada pocível.
+    bool play(unsigned col);
 
     // verifica se é vitória do player1, player2
     GameResult result();
+
+    // calcula a utilidade desse estado do tabuleiro do ponto de vista do player turn==0 (valores mais altos são melhores para ele.)
+    float evaluate()
+    {
+        auto temp_result = result();
+        if(temp_result==GameResult_Player1_Wins) return 100;// melhor caso
+        else if(temp_result==GameResult_Player2_Wins) return -100; // pior caso
+        else if(temp_result==GameResult_Draw) return 0;// tudo igual.
+        else// bora calcular coisas
+        {
+            return 1;// código temporário.
+        }
+    }
 };
-
-
-
--- game_tree.hpp
+++ game_tree.hpp
@@ -1,25 +1,16 @@
 #include <cstdint>
 #include "board.hpp"
 
-struct Node
+struct GameManager
 {
-    GameState board;   // struct que armazena estado do tabuleiro
-    Node *children[4]={nullptr, nullptr, nullptr, nullptr}; // ponteiros dos filhos deste nó
-    uint8_t utility;   // função de utilidade minimax
-};
+    GameState board; // tabuleiro atual do jogo
+    // initializa o tabuleiro.
+    GameManager();
 
-struct GameTree
-{
 
-    // cria a árvore salvando ela em um arquivo, ou carregando de um arquivo.
-    GameTree(bool load = true, const char *filename);
+    // atualiza o tabuleiro usando busca em profundidade como base.
+    void dfs_next(uint8_t depth=16);
 
-    // preenche os campos utility de acordo com o algoritmo minimax
-    void create_minimax();
-
-    // faz busca em profundidade a partir do nó, com máximo `depth` e retorna o primeiro movimento realizado
-    unsigned dfs_next(Node node, uint8_t depth=16);
-
-    // retorna movimento realizado para o nó filho mais favorável de acordo com campo `utility`
-    unsigned minimax_next(Node node, bool turn);
+    // atualiza o tabuleiro do jogo utilizando minimax.
+    void minimax_next(uint8_t depth=16);
 };
