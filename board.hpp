994e1117b9484d2f8a0027636035a36a0af7f7c0
gustavo hanke <gustavo.hanke1@outlook.com>
2023-11-21 11:26:26 -0300

    Changed some things, inplemented the mine max algo.

--- board.hpp
+++ board.hpp
@@ -6,37 +6,69 @@ enum GameResult
     GameResult_NotDone
 };
 
+enum LineFlags
+{
+    Line1 = 0x1,
+    Line2 = 0x2,
+    Line3 = 0x4,
+    Line4 = 0x8,
+    Line5 = 0x10,
+    Line6 = 0x20
+};
+struct BoardArray
+{
+    GameState board[7];
+    unsigned char num_board = 0;
+    void insert(GameState board)
+    {
+        this->board[num_board] = board;
+        num_board++;
+    }
+};
+
 struct GameState
 {
     // cada coluna, usando bit field pra economizar o maximo possivel de memoria
-    unsigned col1 : 4;
-    unsigned col2 : 4;
-    unsigned col3 : 4;
-    unsigned col4 : 4;
+    unsigned col1 : 6;
+    unsigned col2 : 6;
+    unsigned col3 : 6;
+    unsigned col4 : 6;
+    unsigned col5 : 6;
+    unsigned col6 : 6;
+    unsigned col7 : 6;
     // numero de elementos em cada coluna
     unsigned len1 : 3;
     unsigned len2 : 3;
     unsigned len3 : 3;
     unsigned len4 : 3;
-    unsigned turn : 1;       // indica quem deve ser o próximo a jogar.
-    unsigned pading : 3 = 0; // alinhamento de memória, o compilador não gosta de coisas com 29 bits.
+    unsigned len5 : 3;
+    unsigned len6 : 3;
+    unsigned len7 : 3;
+    unsigned turn : 1; // indica quem deve ser o próximo a jogar.
 
-    // joga na coluna especificada, gerando o próximo tabuleiro, retorna falso se não for uma jogada pocível.
-    bool play(unsigned col);
+    // faz  a jogada na coluna e retorna o novo tabuleiro
+    GameState play(unsigned col);
 
     // verifica se é vitória do player1, player2
     GameResult result();
 
-    // calcula a utilidade desse estado do tabuleiro do ponto de vista do player turn==0 (valores mais altos são melhores para ele.)
-    float evaluate()
+    // verifica a validade do tabuleiro
+    bool is_valid()
     {
-        auto temp_result = result();
-        if(temp_result==GameResult_Player1_Wins) return 100;// melhor caso
-        else if(temp_result==GameResult_Player2_Wins) return -100; // pior caso
-        else if(temp_result==GameResult_Draw) return 0;// tudo igual.
-        else// bora calcular coisas
+        return !(len1==7 || len2==7 || len3==7 || len4==7 || len5==7 || len6==7 || len7==7);
+    }
+    // calcula a utilidade desse estado do tabuleiro do ponto de vista do player turn==0 (valores mais altos são melhores para ele.)
+    float evaluate();
+
+    // calcula tabuleiros pocíveis a partir deste tabuleiro
+    BoardArray calculate_sub_boards()
+    {
+        BoardArray final;
+        for (unsigned i = 1; i <= 7; i++)
         {
-            return 1;// código temporário.
+            auto temp_board = play(i);
+            if (temp_board.is_valid())
+                final.insert(temp_board);
         }
     }
 };
--- game_tree.hpp
+++ game_tree.hpp
@@ -1,16 +1,67 @@
 #include <cstdint>
+#include <limits>
 #include "board.hpp"
 
-struct GameManager
+class GameManager
 {
     GameState board; // tabuleiro atual do jogo
+
+    // função de maximisação
+    unsigned char max(BoardArray &boards, uint8_t depth)
+    {
+        unsigned char index=0;
+        float max = -(std::numeric_limits<float>::infinity());
+        for (unsigned char i = 0; i < boards.num_board; i++)
+        {
+            auto temp = evaluate_mine_max(boards.board[i], depth - 1);
+            if (temp > max)
+            {
+                max = temp;
+                index = i;
+            }
+        }
+        return index;
+    }
+        // função de minimisação
+    unsigned char min(BoardArray &boards, uint8_t depth)
+    {
+        unsigned char index=0;
+        float min = (std::numeric_limits<float>::infinity());
+        for (unsigned char i = 0; i < boards.num_board; i++)
+        {
+            auto temp = evaluate_mine_max(boards.board[i], depth - 1);
+            if (temp < min)
+            {
+                min = temp;
+                index = i;
+            }
+        }
+        return index;
+    }
+    // implementação do mine max
+    float evaluate_mine_max(GameState &board, uint8_t depth)
+    {
+        if (!depth || board.result() != GameResult_NotDone)
+            return board.evaluate();
+        auto boards = board.calculate_sub_boards();
+        unsigned char index = 0;
+        if (board.turn == 0)
+            index=max(boards, depth);
+        else
+            index=min(boards, depth);
+        board = boards.board[index];
+    }
+
+public:
     // initializa o tabuleiro.
     GameManager();
 
-
     // atualiza o tabuleiro usando busca em profundidade como base.
-    void dfs_next(uint8_t depth=16);
+    void dfs_next(uint8_t depth = 6);
 
     // atualiza o tabuleiro do jogo utilizando minimax.
-    void minimax_next(uint8_t depth=16);
+    void minimax_next(uint8_t depth = 6)
+    {
+        evaluate_mine_max(board, depth);
+    }
 };
