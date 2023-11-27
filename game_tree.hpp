06a8399c398f965b56de34bf7cbf1318345d4d66
gustavo hanke <gustavo.hanke1@gmail.com>
2023-11-27 08:35:03 -0300

    implemented the alpha beta

--- game_tree.hpp
+++ game_tree.hpp
@@ -6,48 +6,56 @@ struct GameManager
     GameState board; // tabuleiro atual do jogo
 
     // função de maximisação
-    unsigned char max(BoardArray boards, uint8_t depth)
+    unsigned char max(BoardArray boards, uint8_t depth, float alpha, float beta)
     {
         unsigned char index = 0;
         float max = -(std::numeric_limits<float>::infinity());
         for (unsigned char i = 0; i < boards.num_board; i++)
         {
-            auto temp = evaluate_mine_max(boards.board[i], depth - 1);
+            auto temp = evaluate_mine_max(boards.board[i], depth - 1, alpha, beta);
             if (temp > max)
             {
                 max = temp;
                 index = i;
+                if (max > beta)
+                    break; // we found a good enough score
+                if (max > alpha)
+                    alpha = max;
             }
         }
         return index;
     }
     // função de minimisação
-    unsigned char min(BoardArray boards, uint8_t depth)
+    unsigned char min(BoardArray boards, uint8_t depth, float alpha, float beta)
     {
         unsigned char index = 0;
         float min = (std::numeric_limits<float>::infinity());
         for (unsigned char i = 0; i < boards.num_board; i++)
         {
-            auto temp = evaluate_mine_max(boards.board[i], depth - 1);
+            auto temp = evaluate_mine_max(boards.board[i], depth - 1, alpha, beta);
             if (temp < min)
             {
                 min = temp;
                 index = i;
+                if (min < alpha)
+                    break; // we found a bad enough score
+                if (min < beta)
+                    beta = min;
             }
         }
         return index;
     }
     // implementação do mine max
-    float evaluate_mine_max(GameState &board, uint8_t depth)
+    float evaluate_mine_max(GameState &board, uint8_t depth, float alpha=-std::numeric_limits<float>::infinity(), float beta=std::numeric_limits<float>::infinity())
     {
         if (depth == 0 || board.result() != GameResult_NotDone)
             return board.evaluate();
         auto boards = board.calculate_sub_boards();
         unsigned char index = 0;
         if (board.turn == 0)
-            index = max(boards, depth);
+            index = max(boards, depth, alpha, beta);
         else
-            index = min(boards, depth);
+            index = min(boards, depth, alpha, beta);
         board = boards.board[index];
         return board.evaluate();
     }
